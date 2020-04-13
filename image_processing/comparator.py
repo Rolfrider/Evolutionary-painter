9cd3817f0a7d2d6d79d32673511ce557897823be
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-04-13 11:01:59 +0200

    Rename

--- image_processing/comparator.py
+++ image_processing/comparator.py
@@ -11,10 +11,10 @@ class Comparator:
 
     def evaluate(self, individual: List[RGBARect]) -> float:
         image = self.__createImage(individual)
-        diff = ImageChops.difference(self.refImage.convert('RGBA'), image)
-        return 1 - self.__calculatePercentage(diff)
+        diffImage = ImageChops.difference(self.refImage.convert('RGBA'), image)
+        return 1 - self.__calculateDifference(diffImage)
 
-    def __calculatePercentage(self, diffImage: Image) -> float:
+    def __calculateDifference(self, diffImage: Image) -> float:
         pixelsNo = diffImage.size[0] * diffImage.size[1]
         diff = numpy.sum(diffImage) / float(3 * pixelsNo)
         return diff / 255.0
