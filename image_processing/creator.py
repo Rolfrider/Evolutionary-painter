81a94303a1e3121e7baf0e446eae3dcce9596488
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-04-19 22:22:18 +0200

    Fix drwaing logic

--- image_processing/creator.py
+++ image_processing/creator.py
@@ -6,17 +6,12 @@ from evolutionary_algorithm.individual import Individual
 
 def create_image(individual: Individual, width: int, height: int) -> Image:
     final_image = Image.new('RGBA', (width, height))
-    final_draw = ImageDraw.Draw(final_image)
     for rect in map(lambda x: x.rect, individual.data):
-        image = Image.new('RGBA', (rect.w, rect.h))
+        image = Image.new('RGBA', (width, height))
         draw = ImageDraw.Draw(image)
         draw.rectangle(calc_vertexes(rect),
                        (rect.r, rect.g, rect.b, rect.a))
-        final_draw.bitmap(
-            (rect.x, rect.y),
-            image,
-            (rect.r, rect.g, rect.b, rect.a)
-        )
+        final_image = Image.alpha_composite(final_image, image)
     return final_image
 
 
