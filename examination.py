b79251038e8fbbc2372a13ad07b01d5460ea713c
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-04-21 20:18:14 +0200

    Add crossing results

--- examination.py
+++ examination.py
@@ -3,14 +3,14 @@ from experiments.picking import *
 from experiments.rect_number import *
 from experiments.population import *
 
-# examine_mean_strategy(20)
-# examine_interpolate_strategy(20)
+examine_mean_strategy()
+examine_interpolate_strategy()
 
 # examine_best_fitting_strategy(20)
 # examine_roulette_strategy(20)
 # examine_ranking_strategy(20)
 
-examine_rect_number(iter_limit=5)
+# examine_rect_number(iter_limit=5)
 
 # examine_population(iter_limit=5)
 # examine_subpopulation(iter_limit=5)
