63f5b015589c279d516591c2f752a8fae095597f
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-04-22 21:25:08 +0200

    Fix interpolate experiment

--- examination.py
+++ examination.py
@@ -4,7 +4,7 @@ from experiments.rect_number import *
 from experiments.population import *
 
 # examine_mean_strategy()
-# examine_interpolate_strategy()
+examine_interpolate_strategy()
 
 # examine_best_fitting_strategy(20)
 # examine_roulette_strategy(20)
@@ -12,5 +12,5 @@ from experiments.population import *
 
 # examine_rect_number(iter_limit=5)
 
-examine_population(populations=[100])
+# examine_population()
 # examine_subpopulation()
