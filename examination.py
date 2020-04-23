79bbf550c9d53e2dd35c27bf5839282adc85ef47
Rolfrider-Mac <ravkwiatkowski@gmail.com>
2020-04-23 22:31:24 +0200

    Update interpolate crossing

--- examination.py
+++ examination.py
@@ -3,12 +3,12 @@ from experiments.picking import *
 from experiments.rect_number import *
 from experiments.population import *
 
-examine_mean_strategy()
-# examine_interpolate_strategy()
+# examine_mean_strategy()
+examine_interpolate_strategy()
 
-# examine_best_fitting_strategy(20)
-# examine_roulette_strategy(20)
-# examine_ranking_strategy(20)
+# examine_best_fitting_strategy()
+# examine_roulette_strategy()
+# examine_ranking_strategy()
 
 # examine_rect_number(iter_limit=5)
 
